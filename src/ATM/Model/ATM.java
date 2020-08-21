3c910e6e81384bc1a11f598d51f5f91a9046af60
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-08-21 09:16:15 +0200

    Added variables && created setter and getter

--- src/ATM/Model/ATM.java
+++ src/ATM/Model/ATM.java
@@ -1,4 +1,14 @@
 package ATM.Model;
 
 public class ATM {
+    private int[] Bills = new int[3];
+    static int money;
+
+    public void setMoney() {
+        money = Bills[0]*1000+Bills[1]*500+Bills[2]*100;
+    }
+    public int[] getMoney() {
+        setMoney();
+        return new int[]{money, Bills[0], Bills[1], Bills[2]};
+    }
 }
