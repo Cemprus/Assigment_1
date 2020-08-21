fe84251b8bbf8946c1ba576e7282c9c67d55c755
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-08-21 09:17:35 +0200

    Created Constructor

--- src/ATM/Model/ATM.java
+++ src/ATM/Model/ATM.java
@@ -4,6 +4,13 @@ public class ATM {
     private int[] Bills = new int[3];
     static int money;
 
+    public ATM(){
+        Bills[0] = 2;
+        Bills[1] = 3;
+        Bills[2] = 5;
+        setMoney();
+    }
+
     public void setMoney() {
         money = Bills[0]*1000+Bills[1]*500+Bills[2]*100;
     }
