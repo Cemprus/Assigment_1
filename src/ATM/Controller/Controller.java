d1b8578814586519b586f448986464a317e5c0ed
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-08-21 09:22:06 +0200

    Added all important variables from View

--- src/ATM/Controller/Controller.java
+++ src/ATM/Controller/Controller.java
@@ -1,4 +1,29 @@
 package ATM.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label result;
+    @FXML
+    private Label result1;
+    @FXML
+    private Label result2;
+    @FXML
+    private Label result3;
+    @FXML
+    private Label result4;
+    @FXML
+    private Label result5;
+    @FXML
+    private Label result6;
+    @FXML
+    private Label money;
+    @FXML
+    private Label money1;
+    @FXML
+    private Label money2;
+    @FXML
+    private Label money3;
 }
