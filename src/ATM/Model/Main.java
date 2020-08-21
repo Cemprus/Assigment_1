37725e3ba74017b6e823007c5a8ab2a905fc54e4
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-08-21 09:31:07 +0200

    Updated Main function to start with correct variables

--- src/ATM/Model/Main.java
+++ src/ATM/Model/Main.java
@@ -1,18 +1,30 @@
 package ATM.Model;
 
+import ATM.Controller.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../View/sample.fxml"));
+        Pane pane = loader.load();
+        Controller controller = loader.getController();
+
+        ATM atm = new ATM();
+        controller.setAtm(atm);
+        controller.updateMoney();
+
+        Scene scene = new Scene(pane);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("ATM");
         primaryStage.show();
     }
 
